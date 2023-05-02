b75f63d26245800bf8e1e20a6dd58f8f3e883af0
peggy1210 <peggy1210.huang@gmail.com>
2023-05-03 00:48:57 +0800

    Add HW4

---
+++ ML-HW4/Loss.py
@@ -0,0 +1,56 @@
+import numpy as np
+
+
+def compute_BCE_cost(AL, Y):
+    """
+    Implement the binary cross-entropy cost function using the above formula.
+
+    Arguments:
+    AL -- probability vector corresponding to your label predictions, shape (1, number of examples)
+    Y -- true "label" vector (for example: containing 0 if non-cat, 1 if cat), shape (1, number of examples)
+
+    Returns:
+    cost -- binary cross-entropy cost
+    """
+    
+    m = Y.shape[1]
+
+    ### PASTE YOUR CODE HERE ###
+    ### START CODE HERE ###
+
+    e = 1e-5
+    cost = - np.sum(Y*np.log(AL+e)+(1-Y)*np.log(1-AL+e)) / m
+
+    ### END CODE HERE ###
+    
+    cost = np.squeeze(cost)      # To make sure your cost's shape is what we expect (e.g. this turns [[17]] into 17).
+    assert(cost.shape == ())
+    
+    return cost
+
+def compute_CCE_cost(AL, Y):
+    """
+    Implement the categorical cross-entropy cost function using the above formula.
+
+    Arguments:
+    AL -- probability vector corresponding to your label predictions, shape (number of classes, number of examples)
+    Y -- true "label" vector (one hot vector, for example: [[1], [0], [0]] represents rock, [[0], [1], [0]] represents paper, [[0], [0], [1]] represents scissors 
+                              in a Rock-Paper-Scissors image classification), shape (number of classes, number of examples)
+
+    Returns:
+    cost -- categorical cross-entropy cost
+    """
+    
+    m = Y.shape[1]
+
+    # Compute loss from aL and y.
+    ### START CODE HERE ### (≈ 1 line of code)
+    e = 1e-5
+    cost = - np.sum(Y * np.log(AL + e)) / m
+    ### END CODE HERE ###
+    
+    cost = np.squeeze(cost) # To make sure your cost's shape is what we expect (e.g. this turns [[17]] into 17).
+    assert(cost.shape == ())
+    
+    return cost
+
